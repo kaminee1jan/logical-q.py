b34262e4d08cadb97c503c5f8f0b3c423741c6d9
KESANG_SHAKYA <83804073+kaminee1jan@users.noreply.github.com>
2021-08-16 16:15:45 +0530

    Add files via upload

---
+++ nw.py
@@ -0,0 +1,15 @@
+str=input("enter the string")
+print(str.capitalize())
+
+str=input("enter the string")
+print(str.upper())
+
+name ="Kaminee shakya"
+if name.isupper()==False:
+    name=name.upper()
+    print(name)
+
+str="KAMINEE SHAKYA"
+if str.islower()==False:
+    str=str.lower()
+    print(str)
