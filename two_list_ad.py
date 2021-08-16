d74b83dd036f167f83bb78816ba9a02637ef1263
KESANG_SHAKYA <83804073+kaminee1jan@users.noreply.github.com>
2021-08-16 16:16:35 +0530

    Add files via upload

---
+++ two_list_ad.py
@@ -0,0 +1,26 @@
+# list1=[5,6,[],8,9,10]
+# i=0
+# empty_list1=[]
+# empty_list2=[]
+# while i<len(list1):
+#     if list1[i]==[]:
+#         empty_list1.append(list1[i])
+#     else:
+#         empty_list2.append(list1[i])
+#     i=i+1
+# print(empty_list2)
+# print(empty_list1)
+
+# output=[5,6,8,9,10]
+# output[[]]
+
+
+# list1=[-2,-56,-13,20,-6,13]
+# sum=0
+# while i<len(list1):   
+#     if list1[i]<0:
+#         sum=sum+list1[i]
+#     i=i+1
+# print(sum)
+
+
